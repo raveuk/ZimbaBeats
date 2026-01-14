8a4964efa86b60d76fcfd4740a72788230e7bad1
Raveuk <raveuk@live.co.uk>
2026-01-14 13:00:28 +0000

    v1.0.24: Bump version for release with both APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.zimbabeats"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.23"
+        versionCode = 24
+        versionName = "1.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
