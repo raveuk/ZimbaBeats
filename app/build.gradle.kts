d11bebbe0c3bf19f18785be29136ac4c8022dd36
Raveuk <raveuk@users.noreply.github.com>
2026-03-14 23:25:12 +0000

    Bump version to 1.0.63

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.zimbabeats"
         minSdk = 24
         targetSdk = 36
-        versionCode = 62
-        versionName = "1.0.62"
+        versionCode = 63
+        versionName = "1.0.63"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
